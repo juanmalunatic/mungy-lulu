8def73f9a2c3d8e068be904377f0a8b8a539ec59
juanmalunatic <juanmaluna1991@gmail.com>
2019-12-06 10:20:30 -0500

    [Footer] Implement initial version

--- wp-content/themes/mungy-lulu/resources/views/partials/footer.blade.php
+++ wp-content/themes/mungy-lulu/resources/views/partials/footer.blade.php
@@ -1,26 +1,30 @@
-<footer class="content-info" id="footer">
+<footer class="footer content-info">
   <div class="container">
     <div class="row">
-      <div class="col-md-3">
+      <div class="footer__paws"></div>
+      <div class="footer__block footer__block__menu    col-md-3">
         <?php
-          wp_nav_menu(['theme_location' => 'footer_navigation', 'menu_class' => 'nav']);
+          wp_nav_menu([
+            'theme_location' => 'footer_navigation',
+            'menu_class' => 'nav footer__menu'
+          ]);
         ?>
       </div>
-      <div class="col-md-6">
+      <div class="footer__block footer__block__phrase  col-md-6">
         <span class="footer__phrase">
           Love us & leave us
         </span>
       </div>
-      <div class="col-md-3">
+      <div class="footer__block footer__block__address col-md-3">
         <address class="footer__address">
-          <a class="footer__address__block" href="luludogdaycare@gmail.com">
+          <a class="footer__address__block footer__address__block__email" href="luludogdaycare@gmail.com">
             luludogdaycare@gmail.com
           </a>
-          <a class="footer__address__block" href="tel:+14134649200">
+          <a class="footer__address__block footer__address__block__phone" href="tel:+14134649200">
             413-464-9200
           </a>
-          <span class="footer__address__block">
-            1525 West Housatonic St
+          <span class="footer__address__block footer__address__block__address">
+            1525 West Housatonic St <br />
             Pittsfield MA 01201
           </span>
         </address>
