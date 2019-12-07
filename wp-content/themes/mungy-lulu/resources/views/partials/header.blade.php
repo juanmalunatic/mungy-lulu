bf610418cc18fcb3f968944ec1973ad1f395d993
juanmalunatic <juanmaluna1991@gmail.com>
2019-12-07 17:07:52 -0500

    [General] Add initial bs4 navbar

--- wp-content/themes/mungy-lulu/resources/views/partials/header.blade.php
+++ wp-content/themes/mungy-lulu/resources/views/partials/header.blade.php
@@ -1,10 +1,22 @@
 <header class="banner">
   <div class="container">
-    <a class="brand" href="{{ home_url('/') }}">{{ get_bloginfo('name', 'display') }}</a>
-    <nav class="nav-primary">
-      @if (has_nav_menu('primary_navigation'))
-        {!! wp_nav_menu(['theme_location' => 'primary_navigation', 'menu_class' => 'nav']) !!}
-      @endif
+    <nav class="navbar navbar-expand-lg navbar-light">
+      <!-- Logo  -->
+      <a class="navbar-brand" href="{{ home_url('/') }}">{{ get_bloginfo('name', 'display') }}</a>
+      <!-- Toggler -->
+      <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+      </button>
+      <!-- Actual menu -->
+      <div class="collapse navbar-collapse" id="navbarNav">
+        @if (has_nav_menu('primary_navigation'))
+          {!! wp_nav_menu([
+            'theme_location'  => 'primary_navigation',
+            'menu_class'      => 'navbar-nav',
+            'walker'          => new \App\wp_bootstrap4_navwalker(),
+            ]) !!}
+        @endif
+      </div>
     </nav>
   </div>
 </header>
