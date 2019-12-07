9238847ec8979d11e6021aad9f9ab711bc7ca41f
juanmalunatic <juanmaluna1991@gmail.com>
2019-12-07 18:53:39 -0500

    [General] Implement initial header

--- wp-content/themes/mungy-lulu/resources/assets/scripts/routes/common.js
+++ wp-content/themes/mungy-lulu/resources/assets/scripts/routes/common.js
@@ -1,6 +1,12 @@
 export default {
   init() {
     // JavaScript to be fired on all pages
+
+    // X icon from hamburguer
+    $('.hamburger').click(function() {
+      $(this).toggleClass('is-active');
+    });
+
   },
   finalize() {
     // JavaScript to be fired on all pages, after page specific JS is fired
--- wp-content/themes/mungy-lulu/resources/views/partials/header.blade.php
+++ wp-content/themes/mungy-lulu/resources/views/partials/header.blade.php
@@ -1,21 +1,39 @@
-<header class="banner">
+<header class="banner header">
   <div class="container">
-    <nav class="navbar navbar-expand-lg navbar-light">
+    <nav class="navbar navbar-expand-lg">
       <!-- Logo  -->
-      <a class="navbar-brand" href="{{ home_url('/') }}">{{ get_bloginfo('name', 'display') }}</a>
-      <!-- Toggler -->
-      <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
-        <span class="navbar-toggler-icon"></span>
-      </button>
+      <div class="header__logo">
+        <div class="header__logo__image">
+          <a class="navbar-brand" href="{{ home_url('/') }}" title="{{ get_bloginfo('name', 'display') }}">
+            <h1 class="sr-only">
+                {{ get_bloginfo('name', 'display') }}
+            </h1>
+          </a>
+        </div>
+        <div class="header__logo__triangle"></div>
+      </div>
       <!-- Actual menu -->
-      <div class="collapse navbar-collapse" id="navbarNav">
-        @if (has_nav_menu('primary_navigation'))
-          {!! wp_nav_menu([
-            'theme_location'  => 'primary_navigation',
-            'menu_class'      => 'navbar-nav',
-            'walker'          => new \App\wp_bootstrap4_navwalker(),
-            ]) !!}
-        @endif
+      <div class="header__menu">
+        <div class="collapse navbar-collapse" id="navbarNav">
+          @if (has_nav_menu('primary_navigation'))
+            {!! wp_nav_menu([
+              'theme_location'  => 'primary_navigation',
+              'menu_class'      => 'navbar-nav',
+              'walker'          => new \App\wp_bootstrap4_navwalker(),
+              ]) !!}
+          @endif
+        </div>
+      </div>
+      <!-- Toggler (uses both BS and hamburguers) -->
+      <div class="header__toggler">
+        <button class="navbar-toggler hamburger hamburger--spin" type="button"
+          data-toggle="collapse" data-target="#navbarNav"
+          aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation"
+        >
+          <span class="hamburger-box">
+            <span class="hamburger-inner"></span>
+          </span>
+        </button>
       </div>
     </nav>
   </div>
