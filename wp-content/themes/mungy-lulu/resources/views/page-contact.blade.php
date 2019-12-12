1582165e777c9446b558d163a0250268dc99304d
juanmalunatic <juanmaluna1991@gmail.com>
2019-12-12 14:09:48 -0500

    [Contact] Starting up the page

--- wp-content/themes/mungy-lulu/resources/views/page-contact.blade.php
+++ wp-content/themes/mungy-lulu/resources/views/page-contact.blade.php
@@ -1,15 +1,50 @@
 @extends('layouts.app')
 
 @section('content')
-  @while(have_posts()) @php the_post() @endphp
-    @include('partials.page-header')
-    <?php //@include('partials.content-page')?>
-  @endwhile
-  <pre style="background:#f7f; white-space: pre-wrap;">
-    @debug
-  </pre>
+<div class="contact" id="contact">
 
-  TO-DO install gravity forms to implement contact form
+  <div class="contact__area__title">
+    <h1 class="title">
+      {!! get_the_title() !!}
+    </h1>
+  </div>
 
+  <!-- Banner images -->
+  <div class="contact__area__banner">
+    <div class="banner-img banner-img__1"></div>
+    <div class="banner-img banner-img__2"></div>
+    <div class="banner-img banner-img__3"></div>
+  </div>
+
+  <div class="contact__area__form">
+
+    <!-- Chat -->
+    <div class="ca-form__first">
+      @php(dynamic_sidebar('sidebar-contact'))
+    </div>
+
+    <!-- Info -->
+    <div class="ca-form__last">
+      <div class="ca-form__last__text">
+        <h2>For bookings or questions</h2>
+        <span>
+          Please fill out form
+        </span>
+      </div>
+      <div class="ca-form__last__address">
+        <address>
+          <span class="phone">413.464.9200</span>
+          <span class="mail">luludogdaycare@gmail.com</span>
+          <span class="addr">1525 West Housatonit St Pittsfield MA 01201</span>
+        </address>
+      </div>
+      <div class="ca-form__last__social">
+        <a href="#" class="social facebook">F</a>
+        <a href="#" class="social twitter">T</a>
+      </div>
+    </div>
+  </div>
+
+</div>
 @endsection
 
