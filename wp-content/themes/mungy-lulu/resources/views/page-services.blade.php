590bef57fe3a020e54832f534dd61010478d0ee3
juanmalunatic <juanmaluna1991@gmail.com>
2019-12-17 17:52:19 -0500

    [Services] General spacing improvements + add paw

--- wp-content/themes/mungy-lulu/resources/views/page-services.blade.php
+++ wp-content/themes/mungy-lulu/resources/views/page-services.blade.php
@@ -21,11 +21,6 @@
           </a>
         </li>
         <li>
-          <a data-service="hikes" href="#" class="service-toggle">
-            Hikes
-          </a>
-        </li>
-        <li>
           <a data-service="overnight" href="#" class="service-toggle">
             Overnight
           </a>
@@ -35,44 +30,40 @@
     <div class="block block__picture">
       <div class="picture picture__daycare active"></div>
       <div class="picture picture__dog-spa"></div>
-      <div class="picture picture__hikes"></div>
       <div class="picture picture__overnight"></div>
     </div>
     <div class="block block__description">
 
-      <div class="service active service__daycare">
-        <h2> {{$services_svc1_title}} </h2>
-        <p>
-          {!!$service_svc1_content!!}
-        </p>
+      <div class="block__description__content">
+        <div class="service active service__daycare">
+          <h2> {{$services_svc1_title}} </h2>
+          <p>
+            {!!$service_svc1_content!!}
+          </p>
+        </div>
+
+        <div class="service service__dog-spa">
+          <h2> {{$services_svc2_title}} </h2>
+          <p>
+            {!!$service_svc2_content!!}
+          </p>
+        </div>
+
+        <div class="service service__overnight">
+          <h2> {{$services_svc3_title}} </h2>
+          <p>
+            {!!$service_svc3_content!!}
+          </p>
+        </div>
+
+        <div class="book-now">
+          <a href="#">
+            Book now
+          </a>
+        </div>
       </div>
 
-      <div class="service service__dog-spa">
-        <h2> {{$services_svc2_title}} </h2>
-        <p>
-          {!!$service_svc2_content!!}
-        </p>
-      </div>
-
-      <div class="service service__hikes">
-        <h2> {{$services_svc3_title}} </h2>
-        <p>
-          {!!$service_svc3_content!!}
-        </p>
-      </div>
-
-      <div class="service service__overnight">
-        <h2> {{$services_svc4_title}} </h2>
-        <p>
-          {!!$service_svc4_content!!}
-        </p>
-      </div>
-
-      <div class="book-now">
-        <a href="#">
-          Book now
-        </a>
-      </div>
+      <div class="paw"></div>
 
     </div>
   </div>
