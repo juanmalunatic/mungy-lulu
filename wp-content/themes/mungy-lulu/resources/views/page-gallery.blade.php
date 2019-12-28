71fc32e9f5f918443cc0b51ab2c48907357cfc28
Juan Manuel Luna López <lunalopezjm@gmail.com>
2019-12-28 10:49:08 -0500

    [Gallery] Implement QOL changes and JS for it to load

--- wp-content/themes/mungy-lulu/resources/views/page-gallery.blade.php
+++ wp-content/themes/mungy-lulu/resources/views/page-gallery.blade.php
@@ -1,6 +1,9 @@
 @extends('layouts.app')
 
 @section('content')
+<!-- Must be placed before the gallery for it to load properly -->
+<script src="/wp-includes/js/jquery/jquery.js"></script>
+
 <div class="gallery content-wide" id="gallery">
 
   <div class="gallery__area__title">
@@ -8,9 +11,10 @@
     <h2 class="title-secondary"> Meet our friends </h2>
   </div>
 
-  @while (have_posts()) @php the_post() @endphp
-    @include ('partials.content-page')
-  @endwhile
+  <div class="gallery__area__content">
+    @while (have_posts()) @php the_post() @endphp
+      @include ('partials.content-page')
+    @endwhile
+  </div>
 
 @endsection
-
